6cfdf9ef21b130ed6a6675d3423f505a53e7b8cf
fajarfaz <fajarfaz@gmail.com>
2021-06-25 14:41:10 +0700

    search +

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css" />
         <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
         <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <!-- Styles -->
         <style type="text/css">
         body{
@@ -55,14 +55,14 @@
        
     </head>
     <body class="antialiased" >
-        <div class="w-max-xl min-h-screen bg-cover bg-no-repeat bg-center text-gray-500 overflow-y-auto" style="background-image: url({{ asset('image/bk.svg') }});" >
+        <div class="w-max-xl min-h-screen md:px-0 px-2 bg-cover bg-no-repeat bg-center text-gray-500 overflow-y-auto" style="background-image: url({{ asset('image/bk.svg') }});" >
            
-           <div class="grid grid-rows-8 xl:max-w-5xl lg:max-w-4xl md:max-w-2xl m-auto text-center py-12 font-semibold text-lg gap-4 h-full" >
+           <div class="grid grid-rows-8 xl:max-w-5xl lg:max-w-4xl md:max-w-2xl m-auto text-center py-12 font-semibold text-base md:text-lg gap-4 h-full" >
 
            <div class="grid flex-col w-auto space-y-1 max-w-2xl mx-auto gap-4 mb-8" data-aos="fade-up">
-           <div class="flex w-auto flex-row space-x-2 items-center mx-auto">
-                <img src="{{ asset('image/logo2.png') }}" class="w-48" >
-                <label class="text-5xl font-bold text-gray-700 "> Management App</label>
+           <div class="flex w-auto flex-col md:flex-row space-x-0 md:space-x-2 items-center mx-auto">
+                <img src="{{ asset('image/logo2.png') }}" class="w-36 md:w-48" >
+                <label class="text-3xl md:text-5xl font-bold text-gray-700 "> Management App</label>
             </div>
             <label class="tracking-wider ">This is the local homepage for all apps from <span class="text-red-500">24</span><span class="text-blue-900">Slides</span>,<br> Choose the application you need</label>
             <button class="bg-blue-500 shadow-lg text-white rounded-3xl w-max font-semibold mx-auto px-9 tracking-wider py-3 w-auto justify-self-end mt-4 hover:bg-blue-700 duration-300 animate-bounce">Open latest app</button>
@@ -70,39 +70,44 @@
 
            <div class="p-2 bg-gradient-to-tr from-blue-500 to-purple-400 rounded-2xl mx-auto" data-aos="fade-down">
            <div class="row-span-2 rounded-2xl grid grid-rows-4 md:px-10 px-4 pb-0 md:pb-10 bg-white pt-4 bg-cover bg-no-repeat bg-bottom bg-white " >
-             <div class="grid grid-cols-2 items-center justify-items-start">
+             <div class="grid grid-rows-2 md:grid-cols-2 items-center justify-items-start">
                 <div class="flex flex-col text-left border-b">
                   <label class="text-3xl font-black text-gray-700 leading-tight">Application Board</label> 
                   <label class="text-gray-500 tracking-wide">We have 2 App Detected</label> 
               </div>
               <div class="flex flex-row justify-self-end space-x-2 items-center">
-                 <input type="text"  name="" class="bg-gray-50 rounded-full py-1 pl-5 w-48 border-2 tracking-wide font-normal focus:outline-none focus:ring focus:border-blue-300 border-gray-400 duration-300 hover:border-blue-400" placeholder="Search">
-               <div class="bg-blue-400 hover:bg-blue-500 duration-300 cursor-pointer text-white rounded-full w-12 h-12 text-2xl shadow-lg items-center flex items-center">
+                 <input type="search"  id="myInputSearch" onkeyup="searching1()"  class="bg-gray-50 rounded-full py-1 pl-5 w-full md:w-48 border-2 tracking-wide font-normal focus:outline-none focus:ring focus:border-blue-300 border-gray-400 duration-300 hover:border-blue-400" placeholder="Search">
+               <div class="flex-shrink-0 bg-blue-400 hover:bg-blue-500 duration-300 cursor-pointer text-white rounded-full md:w-12 md:h-12 text-xl w-9 h-9 md:text-2xl shadow-lg items-center flex">
                <i class="far fa-bell m-auto"></i>
                 </div>
         
-               <img src="https://images.pexels.com/photos/7853219/pexels-photo-7853219.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="object-cover h-12 w-12 rounded-full shadow-lg">
+               <img src="https://images.pexels.com/photos/7853219/pexels-photo-7853219.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="flex-shrink-0 object-cover w-9 h-9 md:h-12 md:w-12 rounded-full shadow-lg">
               
                
                
               </div>
             </div>
             <div class="overflow-x-auto row-span-3 ">
-             <div class="flex space-x-12 items-center mx-auto px-4 hide-scroll py-6">
-                <div class="bg-gray-700 flex text-white flex-col rounded-xl justify-between tracking-wide  bg-cover bg-no-repeat bg-center shadow-xl h-80 w-64 relative hover-trigger" style="background-image:url({{ asset('image/attend.svg') }});">
-                    <div class="absolute rounded-xl px-4 py-2 hover-target h-full w-full flex  items-center" style="background: #0f0f0f7d;">
+             <div class="flex space-x-6 md:space-x-12 items-center mx-auto px-1 md:px-4 hide-scroll py-6" id="myUL">
+               
+                
+                <li class="flex-shrink-0 bg-gray-700 flex text-white flex-col rounded-xl justify-between tracking-wide  bg-cover bg-no-repeat bg-center shadow-xl h-80 w-64 relative hover-trigger" data-aos="fade-up"
+                data-aos-duration="1500" data-aos-easing="ease-in-sine" style="background-image:url({{ asset('image/attend.svg') }});" > 
+                    <div class="absolute rounded-xl px-4 py-2 hover-target h-full w-full flex  items-center" style="background: #0f0f0f7d;" >
+                        
                         <a href="#" class="relative px-6 py-2 border-4 border-white text-2xl font-bold rounded-xl mx-auto tracking-widest uppercase duration-300 hover:bg-gray-700">Open App
                          <label class="absolute -bottom-7 right-1 text-xs tracking-wide font-normal">
                             <span class="bg-green-400 rounded-full p-1 absolute animate-ping -left-3"> </span>
                             <span class="bg-green-400 rounded-full p-1 absolute -left-3"></span>
                             24 Online Now</label>
                         </a>
+                      
                        
                     </div>
 
                     <label class="absolute capitalize text-sm text-gray-500 -top-5 left-2"> Last Update: <span class="text-gray-700">Today</span></label>
                     <div class="flex-auto flex flex-col rounded-xl px-5 pt-4 font-light text-left text-sm bg-gradient-to-b from-gray-900 to-transparent">
-                    <label class="font-extrabold text-xl tracking-widest uppercase">Attendance App</label>
+                    <h1 class="font-extrabold text-xl tracking-widest uppercase">Attendance App</h1>
                     <label class="text-gray-100 ">Application for work recording</label>
                     </div>
 
@@ -112,11 +117,14 @@
                            <label>Last Login</label>
                            <label class="border-2 rounded-xl px-2 py-1 text-xs">Today</label>
                        </div>
-                   </div>
-                </div>
-                
-                <div class="bg-gray-700 flex text-white flex-col rounded-xl justify-between tracking-wide  bg-cover bg-no-repeat bg-center shadow-xl h-80 w-64 relative hover-trigger" style="background-image:url({{ asset('image/catering.svg') }});">
-                    <div class="absolute rounded-xl px-4 py-2 hover-target h-full w-full flex  items-center" style="background: #0f0f0f7d;">
+                   </div>               
+                </li>
+
+            
+                <li class="flex-shrink-0 bg-gray-700 flex text-white flex-col rounded-xl justify-between tracking-wide  bg-cover bg-no-repeat bg-center shadow-xl h-80 w-64 relative hover-trigger" data-aos="fade-up"
+                data-aos-duration="1500" data-aos-easing="ease-in-sine" style="background-image:url({{ asset('image/catering.svg') }});" > 
+                    <div class="absolute rounded-xl px-4 py-2 hover-target h-full w-full flex  items-center" style="background: #0f0f0f7d;" >
+                         
                         <a href="#" class="relative px-6 py-2 border-4 border-white text-2xl font-bold rounded-xl mx-auto tracking-widest uppercase duration-300 hover:bg-gray-700">Open App
                          <label class="absolute -bottom-7 right-1 text-xs tracking-wide  font-normal">
                             <span class="bg-green-400 rounded-full p-1 absolute animate-ping -left-3"> </span>
@@ -128,7 +136,7 @@
 
                     <label class="absolute capitalize text-sm text-gray-500 -top-5 left-2"> Last Update: <span class="text-gray-700">Today</span></label>
                     <div class="flex-auto flex flex-col rounded-xl px-5 pt-4 font-light text-left text-sm bg-gradient-to-b from-gray-900 to-transparent">
-                    <label class="font-extrabold text-xl tracking-widest uppercase">Catering App</label>
+                    <h1 class="font-extrabold text-xl tracking-widest uppercase">Catering App</h1>
                     <label class="text-gray-100 ">Application for order catering</label>
                     </div>
 
@@ -139,17 +147,18 @@
                            <label class="border-2 rounded-xl text-xs px-2 py-1">18/6/2021</label>
                        </div>
                    </div>
-                </div>
+                </li>
                     
-                  <div class="bg-gray-700 flex text-white flex-col rounded-xl justify-between tracking-wide  bg-cover bg-no-repeat bg-center shadow-xl h-80 w-64 relative py-4 relative" style="background: linear-gradient(330.01deg, rgba(0, 0, 0, 0.104) 5.9%, rgba(0, 0, 0, 0.2) 56.11%, rgba(255, 255, 255, 0.2) 89.06%), #67ABF9;">                   
+                  <li class="flex-shrink-0 bg-gray-700 flex text-white flex-col rounded-xl justify-between tracking-wide  bg-cover bg-no-repeat bg-center shadow-xl h-80 w-64 relative py-4 relative" data-aos="fade-up"
+                data-aos-duration="1500" data-aos-easing="ease-in-sine" style="background: linear-gradient(330.01deg, rgba(0, 0, 0, 0.104) 5.9%, rgba(0, 0, 0, 0.2) 56.11%, rgba(255, 255, 255, 0.2) 89.06%), #67ABF9;">                   
 
                     <label class="absolute capitalize text-sm text-gray-500 -top-5 left-2"> Last Update: <span class="text-gray-700">Today</span></label>
                    
                     <label class="font-bold text-2xl tracking-widest text-center">COMING SOON!</label>      
-                    <label class="font-bold text-xl tracking-widest text-center">Snipe-IT App</label>
+                    <h1 class="font-bold text-xl tracking-widest text-center">Snipe-IT App</h1>
                     <label class="font-bold text-xl tracking-widest text-center text-gray-100">24/06/2021</label>
                    
-                </div>
+                </li>
                
 
              </div>
@@ -163,10 +172,37 @@
             
         
         </div>
+        <script type="text/javascript">
+              function searching1() {
+
+               var input, filter, ul, li, a, i, txtValue;
+               input = document.getElementById("myInputSearch");
+               filter = input.value.toUpperCase();
+               ul = document.getElementById("myUL");
+               li = ul.getElementsByTagName("li");
+               for (i = 0; i < li.length; i++) {
+                a = li[i].getElementsByTagName("h1")[0];
+                txtValue = a.textContent || a.innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    li[i].style.visibility = "visible";
+                    li[i].style.opacity = "1";
+                    li[i].style.display = ""
+                  
+                } else {
+                    li[i].style.visibility = "hidden";
+                    li[i].style.opacity = "0";
+                    li[i].style.transition = "visibility 0s, opacity 0.5s linear";
+                    li[i].style.display = "none"
+                }
+            }
+            }
+        </script>
          <script>
             AOS.init({
               duration: 1200,
           })
+          
+
       </script>
     </body>
 </html>
