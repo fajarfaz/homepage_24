9c207f1a27050c58005fcbce6aa048fa3f6d4f01
aliramadhan <yuukikimisaki@gmail.com>
2021-07-09 09:38:26 +0700

    add backend auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -91,31 +91,31 @@
                 </div>
                 <div class="row-span-3 py-3 relative">
 
-                    <form class="w-full max-w-sm ">
+                    <form class="w-full max-w-sm " method="POST" action="{{ route('login') }}">
+                    @csrf
                       <div class="md:flex md:items-center mb-6">
                         <div class="md:w-1/3">
-                          <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
-                           Email
-                        </label>
+                        <x-jet-label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name" value="{{ __('Email') }}" />
                     </div>
                     <div class="md:w-2/3">
-                      <input class="bg-gray-50 shadow-md appearance-none border-2 border-gray-300 hover:bg-white rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="inline-full-name" type="email" placeholder="email@domain.com">
+                      <x-jet-input class="bg-gray-50 shadow-md appearance-none border-2 border-gray-300 hover:bg-white rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="inline-full-name" id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus placeholder="email@domain.com" />
                   </div>
               </div>
               <div class="md:flex md:items-center mb-6">
                 <div class="md:w-1/3">
                   <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
                     Password
-                </label>
+                  </label>
+                  <x-jet-label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password" value="{{ __('Password') }}" />
                 </div>
                 <div class="md:w-2/3">
-                          <input class="bg-gray-50 shadow-md appearance-none border-2 border-gray-300 hover:bg-white rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="inline-password" type="password" placeholder="******************">
+                          <x-jet-input class="bg-gray-50 shadow-md appearance-none border-2 border-gray-300 hover:bg-white rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="inline-password" type="password" name="password" required autocomplete="current-password" placeholder="******************" />
                       </div>
                   </div>
                   <div class="md:flex md:items-center mb-2 ">
                     <label class="md:w-1/3"> </label>
                     <label class="md:w-2/3 block text-gray-500 font-bold flex items-center">
-                      <input class="mr-2 leading-tight h-4 w-4 rounded-2xl"  type="checkbox">
+                      <x-jet-checkbox class="mr-2 leading-tight h-4 w-4 rounded-2xl" id="remember_me" name="remember" />
                       <span class="text-base">
                             Remember me
                         </span>
@@ -125,14 +125,14 @@
             <div class="md:flex md:items-center">
                 <div class="md:w-1/3"></div>
                 <div class="md:w-2/3">
-                  <button class="shadow-md bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded w-full" type="button">
-                    Sign In
-                </button>
+                  <x-jet-button class="shadow-md bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded w-full">
+                      {{ __('Sign in') }}
+                  </x-jet-button>
             </div>
             </div>
             <div class="bottom-0 absolute flex justify-between items-center w-full space-x-2">
                 <div class="flex-auto border border-gray-300 rounded-xl w-full"></div>
-            <label class="flex-shrink-0 text-base w-auto ">Forgot your Password?<a href="#" class="text-blue-500 hover:text-blue-700 duration-300"> Reset here </a></label>
+            <label class="flex-shrink-0 text-base w-auto ">Forgot your Password?<a href="{{ route('password.request') }}" class="text-blue-500 hover:text-blue-700 duration-300"> Reset here </a></label>
             </div>
             </form>
                 </div>
