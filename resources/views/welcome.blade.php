a735a1b5ca010f0da04edfe5509277b12593978a
fajarfaz <fajarfaz@gmail.com>
2021-07-09 14:13:39 +0700

    jet publish

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -95,7 +95,9 @@
                     @csrf
                       <div class="md:flex md:items-center mb-6">
                         <div class="md:w-1/3">
-                        <x-jet-label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name" value="{{ __('Email') }}" />
+                        <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
+                    Email
+                  </label>
                     </div>
                     <div class="md:w-2/3">
                       <x-jet-input class="bg-gray-50 shadow-md appearance-none border-2 border-gray-300 hover:bg-white rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="inline-full-name" id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus placeholder="email@domain.com" />
@@ -106,7 +108,7 @@
                   <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
                     Password
                   </label>
-                  <x-jet-label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password" value="{{ __('Password') }}" />
+                
                 </div>
                 <div class="md:w-2/3">
                           <x-jet-input class="bg-gray-50 shadow-md appearance-none border-2 border-gray-300 hover:bg-white rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="inline-password" type="password" name="password" required autocomplete="current-password" placeholder="******************" />
@@ -125,7 +127,7 @@
             <div class="md:flex md:items-center">
                 <div class="md:w-1/3"></div>
                 <div class="md:w-2/3">
-                  <x-jet-button class="shadow-md bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded w-full">
+                  <x-jet-button class=" shadow-md bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded w-full text-base">
                       {{ __('Sign in') }}
                   </x-jet-button>
             </div>
